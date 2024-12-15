b66a2fd4b25614c3a24e463931ee09f9150a2820
Chris de la Iglesia <chris@cmdli.dev>
2024-12-14 19:52:19 -0800

    Add missing summons file

---
+++ spells/summons.py
@@ -0,0 +1,65 @@
+import sim.summons
+import sim.feat
+
+
+class FeyWeapon(sim.summons.SummonWeapon):
+    def __init__(self, slot: int, **kwargs):
+        super().__init__(
+            name="FeyWeapon", num_dice=2, die=6, dmg_bonus=3 + slot, **kwargs
+        )
+
+
+class Mirthful(sim.feat.Feat):
+    def __init__(self) -> None:
+        super().__init__()
+        self.used = False
+
+    def begin_turn(self, target: "sim.target.Target"):
+        self.used = False
+
+    def attack_roll(self, args):
+        if not self.used:
+            args.adv = True
+            self.used = True
+
+
+class FeySummon(sim.summons.Summon):
+    def __init__(self, slot: int, caster: "sim.character.Character"):
+        super().__init__(
+            slot=slot,
+            weapon=FeyWeapon(slot, caster=caster),
+            feats=[Mirthful()],
+        )
+
+
+class SummonFey(sim.summons.SummonSpell):
+    def __init__(self, slot: int):
+        super().__init__(
+            "SummonFey",
+            slot,
+        )
+
+    def summon(self, caster: "sim.character.Character"):
+        return FeySummon(self.slot, caster)
+
+
+class CelestialWeapon(sim.summons.SummonWeapon):
+    def __init__(self, slot: int, **kwargs):
+        super().__init__(
+            name="CelestialWeapon", num_dice=2, die=6, dmg_bonus=2 + slot, **kwargs
+        )
+
+
+class CelestialSummon(sim.summons.Summon):
+    def __init__(self, slot: int, caster: "sim.character.Character"):
+        super().__init__(
+            slot=slot, weapon=CelestialWeapon(slot=slot, caster=caster), feats=[]
+        )
+
+
+class SummonCelestial(sim.summons.SummonSpell):
+    def __init__(self, slot: int):
+        super().__init__("SummonCelestial", slot)
+
+    def summon(self, caster: "sim.character.Character"):
+        return CelestialSummon(self.slot, caster)
