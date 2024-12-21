c1208364aab36487d215e271f496ae4e332e0bbd
Chris de la Iglesia <chris@cmdli.dev>
2024-12-20 17:37:20 -0800

    Add basic options to chart.py

--- chart.py
+++ chart.py
@@ -13,15 +13,14 @@ Improvements:
 
 @click.command()
 @click.option("--title", default="Damage per Round", help="Chart title")
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    data = pandas.read_csv("data.csv", header=0)
-    line = px.line(
-        data, x="Level", y="DPR", color="Character", title="Damage per Round"
-    )
+@click.option("--input", required=True, help="Data CSV file")
+def main(title, input):
+    data = pandas.read_csv(input, header=0)
+    line = px.line(data, x="Level", y="DPR", color="Character", title=title)
     line.update_xaxes(dtick=1)
     line.update_yaxes(tick0=0.0, dtick=10)
     line.show()
+
+
+if __name__ == "__main__":
+    main()
