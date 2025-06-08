d1dff7e5ff8b2d33876c34438c73e3e84b4af7d8
Chris de la Iglesia <chris@cmdli.dev>
2025-06-07 22:28:25 -0700

    Version 1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import * as feats from "./feats/index"
-import { Character } from "./sim/Character"
-import { testDPR } from "./sim/Simulation"
-import * as weapons from "./weapons/exports"
-import * as classes from "./classes/index"
-import { Class, Stat, WeaponMastery } from "./sim/types"
-import { Feature } from "./sim/Feature"
-
-export type { Class, Stat, WeaponMastery }
-
-export { Character, feats, weapons, testDPR, classes, Feature }
+export { Character } from "./sim/Character"
+export { testDPR } from "./sim/Simulation"
+export type { Class, Stat, WeaponMastery } from "./sim/types"
+export { Feature } from "./sim/Feature"
